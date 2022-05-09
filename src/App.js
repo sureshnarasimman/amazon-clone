f58e413537d167ff8ac539d11d99728f74ad0a7b
sureshnarasimman <narasimmansuresh07@gmail.com>
2022-05-09 23:28:56 +0530

    navbar is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <Router>
+    <div className="app">
+      <Switch>
+        <Route path='/checkout'><h1>Checkout</h1></Route>
+        <Route path='/login'><h1>Login</h1></Route>
+        <Route path='/' component={Header} />
+        
+      </Switch>
     </div>
+    </Router>
   );
 }
 
