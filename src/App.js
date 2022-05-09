dee2dfd9c18b3a79c2202b5f19c258caa599b6b6
sureshnarasimman <narasimmansuresh07@gmail.com>
2022-05-10 00:09:14 +0530

    banner is done

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import {
   Link
 } from "react-router-dom";
 import Header from './components/Header';
+import Home  from './components/Home';
 
 function App() {
   return (
     <Router>
     <div className="app">
       <Switch>
-        <Route path='/checkout'><h1>Checkout</h1></Route>
+        <Route path='/checkout'>
+        <Header />
+          <h1>Checkout</h1></Route>
         <Route path='/login'><h1>Login</h1></Route>
-        <Route path='/' component={Header} />
-        
+        <Route path='/'>
+          <Header />
+          <Home />
+        </Route>
       </Switch>
     </div>
     </Router>
